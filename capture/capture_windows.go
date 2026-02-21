202a5ca54be2b27ab366fecc724014629c4fc80c
Alex Ballas <alex@ballas.org>
2026-02-21 13:14:42 +0200

    Fix windows attempt

--- capture/capture_windows.go
+++ capture/capture_windows.go
@@ -4,7 +4,7 @@ package capture
 
 /*
 #cgo CXXFLAGS: -std=gnu++20
-#cgo LDFLAGS: -lwindowsapp -ld3d11 -lole32 -lstdc++
+#cgo LDFLAGS: -lwindowsapp -ld3d11 -lole32 -static-libstdc++ -static-libgcc -Wl,-Bstatic -l:libwinpthread.a -Wl,-Bdynamic
 #include "capture_windows.h"
 #include <stdlib.h>
 
